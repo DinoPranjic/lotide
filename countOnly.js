ee1ad74f1de9aab40192f24f3dac83bb49eb275b
DinoPranjic <dinopranjic92@gmail.com>
2022-01-31 20:58:51 +0000

    created a function that will be given an array and an object. It will return an object containing counts of everything that the input object listed.

---
+++ countOnly.js
@@ -0,0 +1,46 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`👍👍👍 Assertion passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToRemove) {
+  let result = {};
+  for (const property in itemsToRemove) {
+    if (itemsToRemove[property] === true) {
+      for (const name of allItems) {
+        if (name === property) {
+          if (result[property]) {
+            result[property] += 1;
+          } else {
+            result[property] = 1;
+          }
+        }
+      }
+    }
+  }
+  return result;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+console.log(countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false }));
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
