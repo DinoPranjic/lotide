047dd19f0fff9e7392dc726aeb4e7fbdb1636048
DinoPranjic <dinopranjic92@gmail.com>
2022-02-01 01:52:27 +0000

    commented out test code

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -16,13 +16,13 @@ const findKeyByValue = function(object, value) {
   return result;
 };
 
+/* TEST CODE
 const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
 
-/* TEST CODE
 console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
 console.log(findKeyByValue(bestTVShowsByGenre, "That '70s Show"));
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
