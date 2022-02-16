cba58ae5381b0d305cd769179f86e8ef8981697e
DinoPranjic <dinopranjic92@gmail.com>
2022-02-16 23:21:26 +0000

    refactored function

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function(firstArray, secondArray) {
-  let isIdentical = true;
-  if (firstArray.length !== secondArray.length) {
-    isIdentical = false;
-  } else {
-    for (let i = 0; i < firstArray.length; i++) {
-      if (firstArray[i] !== secondArray[i]) {
-        isIdentical = false;
-      }
-    }
-  }
-  return isIdentical;
-};
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2) === true) {
-    console.log(`👍👍👍Assertion passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`❌❌❌Assertion failed: ${array1} !== ${array2}`);
-  }
-};
-
 const middle = function(array) {
   let middleOfArray = [];
   if (array.length > 2 && array.length % 2 === 1) {
@@ -31,12 +9,4 @@ const middle = function(array) {
   return middleOfArray;
 };
 
-/* TEST CODE
-middle([1, 2, 3]);
-middle([1, 2, 3, 4, 5]);
-middle([1, 2, 3, 4]);
-middle([1, 2, 3, 4, 5, 6]);
-middle([1]);
-middle([1, 2]);
-assertArraysEqual(middle([1, 2, 3]), middle([1, 2, 3]));
-*/
+module.exports = middle;
