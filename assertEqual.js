626f4eb6fa1f3410fc78867fc58b7131e9718bcd
DinoPranjic <dinopranjic92@gmail.com>
2022-02-16 16:14:11 +0000

    refactored function

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return console.log('👍👍👍 Assertion passed: ' + actual + ' === ' + expected);
+    return console.log(`👍👍👍 Assertion passed: ${actual} === ${expected}`);
   } else {
-    return console.log('❌❌❌ Assertion failed: ' + actual + ' !== ' + expected);
+    return console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected}`);
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// EXPORTS FUNCTION
+module.exports = assertEqual;
