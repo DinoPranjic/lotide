d56844eae93d067d5e922c6ea7d874285ef52cc6
DinoPranjic <dinopranjic92@gmail.com>
2022-02-17 00:22:15 +0000

    updated to use mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,35 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert
 
+describe('#Middle', () => {
+  it('returns [2] for [1, 2, 3]', () => {
+    const numbers = [1, 2, 3];
+    assert.deepEqual(middle(numbers), [2]);
+  });
+
+  it('returns [3] for [1, 2, 3, 4, 5]', () => {
+    const numbers = [1, 2, 3, 4, 5];
+    assert.deepEqual(middle(numbers), [3]);
+  });
+
+  it('returns [2, 3] for [1, 2, 3, 4]', () => {
+    const numbers = [1, 2, 3, 4];
+    assert.deepEqual(middle(numbers), [2, 3]);
+  });
+
+  it('returns ["Lighthouse"] for ["Bootcamp", "Lighthouse", "Labs"]', () => {
+    const words = ["Bootcamp", "Lighthouse", "Labs"];
+    assert.deepEqual(middle(words), ["Lighthouse"]);
+  })
+
+  it('Leaves original array unchanged', () => {
+    const numbers = [1, 2, 3];
+    middle(numbers);
+    assert.deepEqual(numbers, [1, 2, 3]);
+  })
+});
+
+/*
 middle([1, 2, 3]);
 middle([1, 2, 3, 4, 5]);
 middle([1, 2, 3, 4]);
@@ -8,3 +37,4 @@ middle([1, 2, 3, 4, 5, 6]);
 middle([1]);
 middle([1, 2]);
 assertArraysEqual(middle([1, 2, 3]), middle([1, 2, 3]));
+*/
